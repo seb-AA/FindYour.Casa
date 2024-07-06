51e07062420bd246cb3031a6d90d2fd197f94d49
seb-AA <seb@asgeranalytics.com>
2024-07-06 16:53:42 +0200

    code->match db schema

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -21,7 +21,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
   }
 
   const listing = await getListingById(listingId);
-  const reservations = await getReservations(listingId);
+  const reservations = await getReservations({ listingId });
   const currentUser = await getCurrentUser();
 
   if (!listing) {
