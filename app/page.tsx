d962b0e63dea07d58fbf80490ceaf0b64dc8ac1e
seb-AA <seb@asgeranalytics.com>
2024-07-07 12:26:33 +0200

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -22,78 +22,83 @@ const Home = () => {
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 text-center">
-          <h1 className="text-4xl font-bold mb-8">Welcome to FindYour.Casa</h1>
-          <p className="text-lg mb-8">
-            FindYour.Casa is your centralized platform for uploading, analyzing,
-            and comparing properties. Whether you&apos;re considering multiple homes
-            or just want to keep track of potential purchases, FindYour.Casa
-            offers the tools you need to make informed decisions.
-          </p>
-          <h2 className="text-2xl font-semibold mb-4">
-            Get Started in Three Easy Steps:
-          </h2>
-          <ol className="list-decimal list-inside mb-8 text-lg">
-            <li className="mb-4">
-              <span className="font-semibold">Sign Up:</span> Create an account
-              to get started. You can sign up with your email, Google, or GitHub
-              account.
-            </li>
-            <li className="mb-4">
-              <span className="font-semibold">Upload Properties:</span> Add the
-              properties you&apos;re interested in to your profile. Include details
-              like images, descriptions, locations, and prices.
-            </li>
-            <li className="mb-4">
-              <span className="font-semibold">Analyze and Compare:</span> Use
-              our tools to analyze and compare the properties you’ve uploaded.
-              Make the best decision with all the information at your
-              fingertips.
-            </li>
-          </ol>
-          <p className="text-lg mb-8">
-            Ready to get started? Sign up now and start uploading your
-            properties!
-          </p>
-          <div className="flex justify-center gap-4">
-            <button
-              onClick={onLogin}
-              className="
-                flex
-                items-center
-                px-4
-                py-2
-                text-white
-                bg-blue-500
-                rounded-full
-                hover:bg-blue-600
-                transition
-                shadow-md
-                hover:shadow-lg
-              "
-            >
-              <AiOutlineLogin className="mr-2" />
-              Login
-            </button>
-            <button
-              onClick={onRegister}
-              className="
-                flex
-                items-center
-                px-4
-                py-2
-                text-white
-                bg-green-500
-                rounded-full
-                hover:bg-green-600
-                transition
-                shadow-md
-                hover:shadow-lg
-              "
-            >
-              <AiOutlineUserAdd className="mr-2" />
-              Sign Up
-            </button>
+        <div
+          className="pt-10 text-center relative bg-cover bg-center"
+          style={{ backgroundImage: 'url("/images/provence.jpg")' }}
+        >
+          <div className="bg-white bg-opacity-75 p-10 rounded-md shadow-md">
+            <h1 className="text-4xl font-bold mb-8">Welcome to FindYour.Casa</h1>
+            <p className="text-lg mb-8">
+              FindYour.Casa is your centralized platform for uploading, analyzing,
+              and comparing properties. Whether you&apos;re considering multiple regions
+              or just want to keep track of your properties, FindYour.Casa
+              offers the tools you need to make informed decisions.
+            </p>
+            <h2 className="text-2xl font-semibold mb-4">
+              Get Started in Three Easy Steps:
+            </h2>
+            <ol className="list-decimal list-inside mb-8 text-lg">
+              <li className="mb-4">
+                <span className="font-semibold">Sign Up:</span> Create an account
+                to get started. You can sign up with your email, Google, or GitHub
+                account.
+              </li>
+              <li className="mb-4">
+                <span className="font-semibold">Upload Properties:</span> Add the
+                properties you&apos;re interested in or own to your profile. Include details
+                like images, descriptions, locations, prices, with extensive notes section for keeping track of special agreements and more.
+              </li>
+              <li className="mb-4">
+                <span className="font-semibold">Analyze and Compare:</span> Use
+                our tools to analyze and compare the properties you&apos;ve uploaded.
+                Make the best decision with all the information at your
+                fingertips.
+              </li>
+            </ol>
+            <p className="text-lg mb-8">
+              Ready to get started? Sign up now and start uploading your
+              properties!
+            </p>
+            <div className="flex justify-center gap-4">
+              <button
+                onClick={onLogin}
+                className="
+                  flex
+                  items-center
+                  px-4
+                  py-2
+                  text-white
+                  bg-blue-500
+                  rounded-full
+                  hover:bg-blue-600
+                  transition
+                  shadow-md
+                  hover:shadow-lg
+                "
+              >
+                <AiOutlineLogin className="mr-2" />
+                Login
+              </button>
+              <button
+                onClick={onRegister}
+                className="
+                  flex
+                  items-center
+                  px-4
+                  py-2
+                  text-white
+                  bg-green-500
+                  rounded-full
+                  hover:bg-green-600
+                  transition
+                  shadow-md
+                  hover:shadow-lg
+                "
+              >
+                <AiOutlineUserAdd className="mr-2" />
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </Container>
