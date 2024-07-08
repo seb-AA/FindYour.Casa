bcd2ba97d9a076d45a46885620001f7e20f23401
seb-AA <seb@asgeranalytics.com>
2024-07-08 14:49:33 +0200

    fixing listingid

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -100,7 +100,7 @@ const ListingClient: React.FC<IListingClientProps> = ({
 
   return (
     <Container>
-      <div className="max-w-screen-lg mx-auto">
+      <div className="max-w-screen-lg mx-auto mt-20">
         <div className="flex flex-col gap-6">
           <ListingHead
             title={listing.title}
