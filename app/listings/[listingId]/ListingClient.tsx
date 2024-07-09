fc8b376448d1843d21d9c4da5a10f80314484faa
seb-AA <seb@asgeranalytics.com>
2024-07-09 16:45:42 +0200

    map

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -3,7 +3,6 @@
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
-import Map from "@/app/components/Map";
 import { categories } from "@/app/components/navbar/Categories";
 import { Listing, Reservation, User } from "@prisma/client";
 import { useMemo } from "react";
@@ -27,33 +26,43 @@ const ListingClient: React.FC<IListingClientProps> = ({
 
   return (
     <Container>
-      <div className="flex flex-col gap-6 w-full">
-        <ListingHead
-          title={listing.title}
-          imageSrc={listing.imageSrc}
-          locationValue={listing.locationValue}
-          id={listing.id}
-          currentUser={currentUser}
-        />
-        <div className="grid grid-cols-1 gap-6 w-full">
-          <ListingInfo
-            user={listing.user}
-            category={category}
-            description={listing.description}
-            roomCount={listing.roomCount}
-            guestCount={listing.guestCount}
-            bathroomCount={listing.bathroomCount}
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
             locationValue={listing.locationValue}
-            agentWebsite={listing.agentWebsite || undefined}
-            notes={listing.notes || undefined}
-            hasSwimmingPool={listing.hasSwimmingPool !== null ? listing.hasSwimmingPool : undefined}
-            hasGarage={listing.hasGarage !== null ? listing.hasGarage : undefined}
-            numberOfOtherBuildings={listing.numberOfOtherBuildings !== null ? listing.numberOfOtherBuildings : undefined}
-            numberOfHabitableBuildings={listing.numberOfHabitableBuildings !== null ? listing.numberOfHabitableBuildings : undefined}
-            landSize={listing.landSize !== null ? listing.landSize : undefined}
-            arableLandSize={listing.arableLandSize !== null ? listing.arableLandSize : undefined}
+            id={listing.id}
+            currentUser={currentUser}
           />
-          <Map center={listing.locationValue ? JSON.parse(listing.locationValue) : undefined} />
+          <div
+            className="
+            grid
+            grid-cols-1
+            md:grid-cols-7
+            md:gap-10
+            gap-6
+            align-center
+          "
+          >
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+              agentWebsite={listing.agentWebsite || undefined}
+              notes={listing.notes || undefined}
+              hasSwimmingPool={listing.hasSwimmingPool !== null ? listing.hasSwimmingPool : undefined}
+              hasGarage={listing.hasGarage !== null ? listing.hasGarage : undefined}
+              numberOfOtherBuildings={listing.numberOfOtherBuildings !== null ? listing.numberOfOtherBuildings : undefined}
+              numberOfHabitableBuildings={listing.numberOfHabitableBuildings !== null ? listing.numberOfHabitableBuildings : undefined}
+              landSize={listing.landSize !== null ? listing.landSize : undefined}
+              arableLandSize={listing.arableLandSize !== null ? listing.arableLandSize : undefined}
+            />
+          </div>
         </div>
       </div>
     </Container>
