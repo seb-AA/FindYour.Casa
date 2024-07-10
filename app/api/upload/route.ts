6d8585acb822128a842bf9df878e000d5fdbf3ac
seb-AA <seb@asgeranalytics.com>
2024-07-10 10:24:48 +0200

    lisitngid patch method

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import formidable from 'formidable';
+import formidable, { File as FormidableFile } from 'formidable';
 import fs from 'fs';
 import path from 'path';
 
@@ -9,18 +9,16 @@ const uploadDir = path.join(process.cwd(), '/public/uploads');
 // Ensure the directory exists
 fs.mkdirSync(uploadDir, { recursive: true });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// Export runtime configuration
+export const runtime = 'nodejs';
 
 export async function POST(request: Request) {
   return new Promise((resolve, reject) => {
     const form = formidable({ multiples: true, uploadDir });
 
     form.on('fileBegin', (name, file) => {
-      file.path = path.join(uploadDir, file.name);
+      const formidableFile = file as unknown as FormidableFile;
+      formidableFile.path = path.join(uploadDir, formidableFile.name || '');
     });
 
     form.parse(request, (err, fields, files) => {
