803baa8a2cee6d6fa96e20757c11a3b93037f24c
seb-AA <seb@asgeranalytics.com>
2024-07-10 10:08:56 +0200

    lisitngid patch method

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,27 +1,30 @@
+import { NextResponse } from 'next/server';
 import formidable from 'formidable';
 import fs from 'fs';
 import path from 'path';
-
-const uploadDir = path.join(process.cwd(), '/public/uploads');
-
-export async function POST(req, res) {
-  const form = formidable({ multiples: true, uploadDir });
-
-  form.on('fileBegin', (name, file) => {
-    file.path = path.join(uploadDir, file.name);
-  });
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      res.status(500).json({ error: 'Something went wrong' });
-      return;
-    }
-    res.status(200).json({ files });
-  });
-}
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
+
+const uploadDir = path.join(process.cwd(), '/public/uploads');
+
+export async function POST(request: Request) {
+  return new Promise((resolve, reject) => {
+    const form = formidable({ multiples: true, uploadDir });
+
+    form.on('fileBegin', (name, file) => {
+      file.path = path.join(uploadDir, file.name);
+    });
+
+    form.parse(request, (err, fields, files) => {
+      if (err) {
+        reject(NextResponse.json({ error: 'Something went wrong' }, { status: 500 }));
+        return;
+      }
+      resolve(NextResponse.json({ files }));
+    });
+  });
+}
