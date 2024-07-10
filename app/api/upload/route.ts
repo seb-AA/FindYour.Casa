38eac2fe35bfce0c39c4dac75cb13e602ab8d0e3
seb-AA <seb@asgeranalytics.com>
2024-07-10 09:46:46 +0200

    lisitngid patch method

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,32 +2,26 @@ import formidable from 'formidable';
 import fs from 'fs';
 import path from 'path';
 
+const uploadDir = path.join(process.cwd(), '/public/uploads');
+
+export async function POST(req, res) {
+  const form = formidable({ multiples: true, uploadDir });
+
+  form.on('fileBegin', (name, file) => {
+    file.path = path.join(uploadDir, file.name);
+  });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.status(500).json({ error: 'Something went wrong' });
+      return;
+    }
+    res.status(200).json({ files });
+  });
+}
+
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-const uploadDir = path.join(process.cwd(), '/public/uploads');
-
-const handler = async (req, res) => {
-  if (req.method === 'POST') {
-    const form = formidable({ multiples: true, uploadDir });
-
-    form.on('fileBegin', (name, file) => {
-      file.path = path.join(uploadDir, file.name);
-    });
-
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        res.status(500).json({ error: 'Something went wrong' });
-        return;
-      }
-      res.status(200).json({ files });
-    });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-};
-
-export default handler;
