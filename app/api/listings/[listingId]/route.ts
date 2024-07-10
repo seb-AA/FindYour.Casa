dfcb3b6a55a39680c45470affa849de71dcc593a
seb-AA <seb@asgeranalytics.com>
2024-07-10 09:19:45 +0200

    lisitngid patch method

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -35,3 +35,39 @@ export async function DELETE(request: Request, { params }: { params: IListingPar
 
   return NextResponse.json(listing);
 }
+
+export async function PATCH(request: Request, { params }: { params: IListingParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+  const data = await request.json();
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  // Convert listingId to number
+  const numericListingId = Number(listingId);
+
+  if (isNaN(numericListingId)) {
+    throw new Error("Invalid listingId format");
+  }
+
+  try {
+    const listing = await prisma.listing.updateMany({
+      where: {
+        id: numericListingId,
+        userId: currentUser.id,
+      },
+      data,
+    });
+
+    return NextResponse.json(listing);
+  } catch (error) {
+    return NextResponse.error({ status: 500, body: { error: error.message } });
+  }
+}
