55f1c3f038e478252b6029a9abc475a9b4cbc89d
seb-AA <seb@asgeranalytics.com>
2024-07-10 10:32:35 +0200

    lisitngid patch method

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,4 +1,5 @@
-import { NextResponse } from 'next/server';
+import nextConnect from 'next-connect';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import formidable, { File as FormidableFile } from 'formidable';
 import fs from 'fs';
 import path from 'path';
@@ -9,24 +10,36 @@ const uploadDir = path.join(process.cwd(), '/public/uploads');
 // Ensure the directory exists
 fs.mkdirSync(uploadDir, { recursive: true });
 
-// Export runtime configuration
-export const runtime = 'nodejs';
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
 
-export async function POST(request: Request) {
-  return new Promise((resolve, reject) => {
-    const form = formidable({ multiples: true, uploadDir });
+const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry, something went wrong! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
 
-    form.on('fileBegin', (name, file) => {
-      const formidableFile = file as unknown as FormidableFile;
-      formidableFile.filepath = path.join(uploadDir, formidableFile.originalFilename || '');
-    });
+apiRoute.post((req, res) => {
+  const form = formidable({ multiples: true, uploadDir });
 
-    form.parse(request, (err, fields, files) => {
-      if (err) {
-        reject(NextResponse.json({ error: 'Something went wrong' }, { status: 500 }));
-        return;
-      }
-      resolve(NextResponse.json({ files }));
-    });
+  form.on('fileBegin', (name, file) => {
+    const formidableFile = file as unknown as FormidableFile;
+    formidableFile.filepath = path.join(uploadDir, formidableFile.originalFilename || '');
   });
-}
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.status(500).json({ error: 'Something went wrong' });
+      return;
+    }
+    res.status(200).json({ files });
+  });
+});
+
+export default apiRoute;
