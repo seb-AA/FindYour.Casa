ed34d613c0663a5ab0d2822ca8768f63293884cf
seb-AA <seb@asgeranalytics.com>
2024-07-11 08:51:19 +0200

    ?

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -27,7 +27,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     if (!currentUser) {
       return loginModal.onOpen();
     }
-    rentModal.onOpen('add');
+    rentModal.onOpen("add");
   }, [currentUser, loginModal, rentModal]);
 
   return (
