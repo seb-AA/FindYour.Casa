529c91d1309ff913d581e4ed5f77abc09047d9f2
seb-AA <seb@asgeranalytics.com>
2024-07-11 08:39:22 +0200

    ?

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -129,7 +129,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
           {location?.region}, {location?.label}
         </div>
         <div className="font-light text-neutral-500">
-          {reservationDate || `${data.category} - ${data.radius ?? 0} km radius`}
+          {reservationDate || `${data.category} - ${data.radius || 0} km radius`}
         </div>
         <div className="flex flex-row items-center gap-1">
           <div className="font-semibold">$ {price}</div>
