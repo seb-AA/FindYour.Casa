e904dba6f7613bbc2905bf4d9e4c4d910a31bc39
seb-AA <seb@asgeranalytics.com>
2024-07-12 09:02:16 +0200

    ?

--- app/hooks/useCountries.ts
+++ app/hooks/useCountries.ts
@@ -1,5 +1,4 @@
 import countries from "world-countries";
-import axios from "axios";
 
 const formattedCountries = countries.map((country: any) => ({
   label: country.name.common,
@@ -19,23 +18,9 @@ const useCountries = () => {
     return country;
   };
 
-  const getByLatLng = async (latitude: number, longitude: number) => {
-    const apiKey = process.env.NEXT_PUBLIC_OPENCAGE_API_KEY;
-    const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`);
-
-    const { city, state, country } = response.data.results[0].components;
-
-    return {
-      city,
-      region: state,
-      country,
-    };
-  };
-
   return {
     getAll,
     getByValue,
-    getByLatLng,
   };
 };
 
