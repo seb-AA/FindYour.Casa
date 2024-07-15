89b2dcf13dcd67691c91c0c16ec9a2fa3af44b4a
seb-AA <seb@asgeranalytics.com>
2024-07-15 17:42:53 +0200

    refactor

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { NextAuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth/core/types";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -18,7 +18,7 @@ interface CustomSession {
   expires: string;
 }
 
-const authOptions: NextAuthOptions = {
+const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
