ff97b51dd733585ec0c88f078ad32e634c975938
seb-AA <seb@asgeranalytics.com>
2024-07-15 15:15:24 +0200

    refactor

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import authOptions from "@/pages/api/auth/[...nextauth]"; // Use default import
 import prisma from "@/app/libs/prismadb";
 import { User } from "@prisma/client";
 import { Session } from "next-auth";
