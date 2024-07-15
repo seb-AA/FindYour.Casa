76131de74505c3b0067f0b32ef948f866671fd51
seb-AA <seb@asgeranalytics.com>
2024-07-15 15:20:21 +0200

    refactor

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import { JWT } from "next-auth/jwt";
 import { Session } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
